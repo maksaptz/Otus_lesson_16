45e1a3358f5d66589281c8801fcfc333100c1c99
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-09-06 18:45:10 +0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -14,7 +14,6 @@ sed -i 's/#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
 sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/' /etc/rsyslog.conf
 sed -i 's/#module(load="imtcp")/module(load="imtcp")/' /etc/rsyslog.conf
 sed -i 's/#input(type="imtcp" port="514")/input(type="imtcp" port="514")/' /etc/rsyslog.conf
-echo '
 ###Добавляем список разрешенных IP
 $AllowedSender UDP, 192.168.50.11/24, [::1]/128
 $AllowedSender TCP, 192.168.50.11/24, [::1]/128
@@ -23,5 +22,3 @@ $template RemInputLogs, "/var/spool/rsyslog/%FROMHOST-IP%/%PROGRAMNAME%.log"
 *.* ?RemInputLogs' >> /etc/rsyslog.conf
 ###Перезагружаем службу
 systemctl restart rsyslog
-##grep nginx_conf /var/log/audit/audit.log
-##ls /var/spool/rsyslog/192.168.50.11/
